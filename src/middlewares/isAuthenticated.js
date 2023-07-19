d40ce267f35b19f4c8ef863b8102f891aa172a02
sonnyrmt <santi2357@hotmail.com>
2023-07-18 21:14:55 -0300

    feat: middleware auth

---
+++ src/middlewares/isAuthenticated.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../services";
+require("dotenv").config();
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) throw new Error();
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    if (!user) return res.status(401).end();
+
+    delete user.password;
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Authentication required" });
+  }
+};
+
+export default isAuthenticated;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import isAuthenticated from "../middlewares/isAuthenticated";
 import { registerRoutes, loginRoutes } from "./authentication";
 
 const { Router } = require("express");
@@ -5,5 +6,6 @@ const router = Router();
 
 router.use("/login", loginRoutes);
 router.use("/register", registerRoutes);
+router.use("/test", isAuthenticated, (req, res) => res.status(200).send("ok"));
 
 export default router;
